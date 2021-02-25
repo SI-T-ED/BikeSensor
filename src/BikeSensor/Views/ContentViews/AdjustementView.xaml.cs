41ad9be3d490278c1baa3bad2bbf74baa17447c4
DimitriTimoz <45998396+DimitriTimoz@users.noreply.github.com>
2021-02-25 20:00:04 +0100

    c

---
+++ src/BikeSensor/Views/ContentViews/AdjustementView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace BikeSensor.Views.ContentViews
+{
+    public partial class AdjustementView : ContentView
+    {
+        public AdjustementView()
+        {
+            InitializeComponent();
+        }
+    }
+}
