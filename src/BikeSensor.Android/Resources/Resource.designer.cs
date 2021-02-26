43f776f46691b3f40652d344ce86b7d89cea83c0
DimitriTimoz <45998396+DimitriTimoz@users.noreply.github.com>
2021-02-26 14:56:20 +0100

    Settings UI

--- src/BikeSensor.Android/Resources/Resource.designer.cs
+++ src/BikeSensor.Android/Resources/Resource.designer.cs
@@ -11797,13 +11797,19 @@ namespace BikeSensor.Droid
 			public const int Settings = 2131165330;
 			
 			// aapt resource value: 0x7F070093
-			public const int test_custom_background = 2131165331;
+			public const int smartphone = 2131165331;
 			
 			// aapt resource value: 0x7F070094
-			public const int tooltip_frame_dark = 2131165332;
+			public const int test_custom_background = 2131165332;
 			
 			// aapt resource value: 0x7F070095
-			public const int tooltip_frame_light = 2131165333;
+			public const int tooltip_frame_dark = 2131165333;
+			
+			// aapt resource value: 0x7F070096
+			public const int tooltip_frame_light = 2131165334;
+			
+			// aapt resource value: 0x7F070097
+			public const int wifi = 2131165335;
 			
 			static Drawable()
 			{
